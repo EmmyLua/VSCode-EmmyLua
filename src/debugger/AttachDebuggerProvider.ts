46c6e042748cf661d5ce77ad50e93b031ae4363e
tangzx <272669294@qq.com>
2018-05-25 15:01:25 +0800

    list processes

--- src/debugger/AttachDebuggerProvider.ts
+++ src/debugger/AttachDebuggerProvider.ts
@@ -3,6 +3,19 @@
 import * as vscode from 'vscode';
 import { WorkspaceFolder, DebugConfiguration, CancellationToken, ProviderResult } from 'vscode';
 import { savedContext } from '../extension';
+import * as cp from "child_process";
+import { basename } from 'path';
+
+var parseString = require('xml2js').parseString;
+
+interface ProcessInfoItem extends vscode.QuickPickItem {
+    pid: number;
+}
+
+interface AttachDebugConfiguration extends DebugConfiguration {
+    pid: number;
+    extensionPath: string;
+}
 
 export class AttachDebuggerProvider implements vscode.DebugConfigurationProvider {
 
@@ -16,9 +29,36 @@ export class AttachDebuggerProvider implements vscode.DebugConfigurationProvider
         return [config];
     }
 
-    resolveDebugConfiguration(folder: WorkspaceFolder | undefined, debugConfiguration: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+    resolveDebugConfiguration(folder: WorkspaceFolder | undefined, debugConfiguration: AttachDebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
         debugConfiguration.extensionPath = savedContext.extensionPath;
-        return debugConfiguration;
+        
+        return new Promise((resolve, reject) => {
+            const args = [`${savedContext.extensionPath}/server/windows/x86/emmy.arch.exe`, " ", "list_processes"];
+            cp.exec(args.join(" "), (err, stdout, stderr) => {
+                parseString(stdout, function (err:any, result:any) {
+                    const items = <ProcessInfoItem[]> result.list.process.map((data:any) => {
+                        const pid = data["$"].pid;
+                        const title = data.title[0];
+                        const path = data.path[0];
+                        const name = basename(path);
+                        return <ProcessInfoItem> {
+                            pid: pid,
+                            label: `${pid} : ${name}`,
+                            description: title,
+                            detail: path
+                        };
+                    });
+                    vscode.window.showQuickPick(items).then((item: ProcessInfoItem | undefined) => {
+                        if (item) {
+                            debugConfiguration.pid = item.pid;
+                            resolve(debugConfiguration);
+                        } else {
+                            reject();
+                        }
+                    });
+                });
+            }).on("error", error => reject);
+        });
     }
 
     dispose() {
