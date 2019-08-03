c12552e0a94ac72aab012234036d99b3d59fae9f
tangzx <love.tangzx@qq.com>
2019-08-03 11:09:35 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,30 @@
+mkdir -p temp
+
+DOWNLOAD_URL="https://github.com/EmmyLua/EmmyLuaDebugger/releases/download"
+EMMY_DEBUGGER_VERSION="1.0.1"
+
+function download() {
+    echo "download emmy_core.so"
+    wget "${DOWNLOAD_URL}/${EMMY_DEBUGGER_VERSION}/emmy_core.so" -O temp/emmy_core.so
+    echo "download emmy_core@x86.zip"
+    wget "${DOWNLOAD_URL}/${EMMY_DEBUGGER_VERSION}/emmy_core@x86.zip" -O temp/emmy_core@x86.zip
+    echo "download emmy_core@x64.zip"
+    wget "${DOWNLOAD_URL}/${EMMY_DEBUGGER_VERSION}/emmy_core@x64.zip" -O temp/emmy_core@x64.zip
+}
+
+function extract() {
+    mkdir -p debugger/emmy/unix
+    cp temp/emmy_core.so debugger/emmy/unix/emmy_core.so
+
+    unzip -o "temp/emmy_core@x86.zip" lib/emmy_core.dll -d temp/x86
+    mkdir -p debugger/emmy/windows/x86
+    cp temp/x86/lib/emmy_core.dll debugger/emmy/windows/x86/emmy_core.dll
+
+    unzip -o "temp/emmy_core@x64.zip" lib/emmy_core.dll -d temp/x64
+    mkdir -p debugger/emmy/windows/x64
+    cp temp/x64/lib/emmy_core.dll debugger/emmy/windows/x64/emmy_core.dll
+}
+
+download
+extract
+vsce package
