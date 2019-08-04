a962b1946361a19d51c671b628b3e88aaeecfab4
tangzx <love.tangzx@qq.com>
2019-08-04 18:21:08 +0800

    clean code

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-mkdir -p temp
-
-DOWNLOAD_URL="https://github.com/EmmyLua/EmmyLuaDebugger/releases/download"
-EMMY_CORE_VERSION="1.0.1"
+EMMY_DEBUGGER_DOWNLOAD_URL="https://github.com/EmmyLua/EmmyLuaDebugger/releases/download"
+EMMY_DEBUGGER_VERSION="1.0.1"
 LS_DOWNLOAD_URL="https://github.com/EmmyLua/EmmyLua-LanguageServer/releases/download"
 LS_VERSION="0.3.0"
 LEGACY_DEBUGGER_DOWNLOAD_URL="https://github.com/EmmyLua/EmmyLuaLegacyDebugger/releases/download"
@@ -9,11 +7,11 @@ LEGACY_DEBUGGER_VERSION="1.0.0"
 
 function download() {
     echo "download emmy_core.so"
-    wget "${DOWNLOAD_URL}/${EMMY_CORE_VERSION}/emmy_core.so" -O temp/emmy_core.so
+    wget "${EMMY_DEBUGGER_DOWNLOAD_URL}/${EMMY_DEBUGGER_VERSION}/emmy_core.so" -O temp/emmy_core.so
     echo "download emmy_core@x86.zip"
-    wget "${DOWNLOAD_URL}/${EMMY_CORE_VERSION}/emmy_core@x86.zip" -O temp/emmy_core@x86.zip
+    wget "${EMMY_DEBUGGER_DOWNLOAD_URL}/${EMMY_DEBUGGER_VERSION}/emmy_core@x86.zip" -O temp/emmy_core@x86.zip
     echo "download emmy_core@x64.zip"
-    wget "${DOWNLOAD_URL}/${EMMY_CORE_VERSION}/emmy_core@x64.zip" -O temp/emmy_core@x64.zip
+    wget "${EMMY_DEBUGGER_DOWNLOAD_URL}/${EMMY_DEBUGGER_VERSION}/emmy_core@x64.zip" -O temp/emmy_core@x64.zip
     echo "download EmmyLua-LS-all.jar"
     wget "${LS_DOWNLOAD_URL}/${LS_VERSION}/EmmyLua-LS-all.jar" -O temp/EmmyLua-LS-all.jar
     echo "download legacy debugger"
@@ -43,6 +41,7 @@ function extract() {
     unzip -o "temp/legacyDebugger.zip" -d debugger/windows
 }
 
+mkdir -p temp
 download
 extract
 vsce package
