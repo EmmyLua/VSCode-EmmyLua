4e45b60fc887f3163358d528a1574bcabc89622b
tangzx <love.tangzx@qq.com>
2019-08-05 23:54:33 +0800

    prepare version

--- prepare-version.js
+++ prepare-version.js
@@ -5,6 +5,8 @@ const version = process.env['APPVEYOR_BUILD_VERSION']
 if (version) {
     var text = fs.readFileSync('package.json').toString();
     const reg = /\"version\":\s*\"(.+?)\",/;
-    text = text.replace(reg, '"version": "3.3.3",');
+    text = text.replace(reg, `"version": "${version}",`);
     fs.writeFileSync('package.json', text);
+
+    console.log(`set version to: ${version}`)
 }
