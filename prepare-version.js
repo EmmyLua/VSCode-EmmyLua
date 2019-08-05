d5a6206fe7819b617452d5a186d6c858ade4ed31
tangzx <love.tangzx@qq.com>
2019-08-06 00:14:17 +0800

    prepare version

--- prepare-version.js
+++ prepare-version.js
@@ -1,12 +1,22 @@
 const fs = require("fs")
 
-const version = process.env['APPVEYOR_BUILD_VERSION']
+if (process.env['CI']) {
+    var version;
+    if (process.env['TRAVIS']) {
+        const tag = process.env['TRAVIS_TAG']
+        version = tag
+    }
+    else if (process.env['APPVEYOR']) {
+        const tag = process.env['APPVEYOR_REPO_TAG_NAME']
+        version = tag || process.env['APPVEYOR_BUILD_VERSION']
+    }
 
-if (version) {
-    var text = fs.readFileSync('package.json').toString();
-    const reg = /\"version\":\s*\"(.+?)\",/;
-    text = text.replace(reg, `"version": "${version}",`);
-    fs.writeFileSync('package.json', text);
-
-    console.log(`set version to: ${version}`)
+    if (version) {
+        var text = fs.readFileSync('package.json').toString();
+        const reg = /\"version\":\s*\"(.+?)\",/;
+        text = text.replace(reg, `"version": "${version}",`);
+        fs.writeFileSync('package.json', text);
+    
+        console.log(`set version to: ${version}`)
+    }
 }
