2f4b3b250bc0ed9e639fecce1ff64040aa3abd77
tangzx <love.tangzx@qq.com>
2019-09-28 11:45:17 +0800

    build script

--- build/config.js
+++ build/config.js
@@ -1,8 +1,6 @@
 exports.default = {
-    emmyDebuggerVersion: '1.0.6',
+    emmyDebuggerVersion: '1.0.9',
     emmyDebuggerUrl: 'https://github.com/EmmyLua/EmmyLuaDebugger/releases/download',
-    legacyDebuggerVersion: '1.0.1',
-    legacyDebuggerUrl: 'https://github.com/EmmyLua/EmmyLuaLegacyDebugger/releases/download',
     lanServerVersion: "0.3.1",
     lanServerUrl: 'https://github.com/EmmyLua/EmmyLua-LanguageServer/releases/download'
 }
--- build/prepare.js
+++ build/prepare.js
@@ -19,7 +19,6 @@ async function downloadDepends() {
         downloadTo(`${config.emmyDebuggerUrl}/${config.emmyDebuggerVersion}/emmy_core@x86.zip`, 'temp/emmy_core@x86.zip'),
         downloadTo(`${config.emmyDebuggerUrl}/${config.emmyDebuggerVersion}/emmy_core@x64.zip`, 'temp/emmy_core@x64.zip'),
         downloadTo(`${config.lanServerUrl}/${config.lanServerVersion}/EmmyLua-LS-all.jar`, 'temp/EmmyLua-LS-all.jar'),
-        downloadTo(`${config.legacyDebuggerUrl}/${config.legacyDebuggerVersion}/debugger.zip`, 'temp/debugger.zip'),
     ]);
 }
 
@@ -37,17 +36,11 @@ async function build() {
     await fc('temp/emmy_core.so', 'debugger/emmy/linux/emmy_core.so', { mkdirp: true });
 
     // win
-    await decompress('temp/emmy_core@x86.zip', 'temp/x86');
-    await fc('temp/x86/emmy_core.dll', 'debugger/emmy/windows/x86/emmy_core.dll', { mkdirp: true });
-
-    await decompress('temp/emmy_core@x64.zip', 'temp/x64');
-    await fc('temp/x64/emmy_core.dll', 'debugger/emmy/windows/x64/emmy_core.dll', { mkdirp: true });
+    await decompress('temp/emmy_core@x86.zip', 'debugger/emmy/windows/x86/');
+    await decompress('temp/emmy_core@x64.zip', 'debugger/emmy/windows/x64/');
 
     // ls
     await fc('temp/EmmyLua-LS-all.jar', 'server/EmmyLua-LS-all.jar', { mkdirp: true });
-    
-    // leagcy debugger
-    await decompress('temp/debugger.zip', 'debugger/windows');
 }
 
 build().catch(console.error);
