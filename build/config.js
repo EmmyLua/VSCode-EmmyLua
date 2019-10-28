0baa3410d05cbe989198ebae0ac8ff8cb2085731
tangzx <love.tangzx@qq.com>
2019-10-28 23:21:44 +0800

    ls version = 0.3.4

--- build/config.js
+++ build/config.js
@@ -1,6 +1,6 @@
 exports.default = {
     emmyDebuggerVersion: '1.0.12',
     emmyDebuggerUrl: 'https://github.com/EmmyLua/EmmyLuaDebugger/releases/download',
-    lanServerVersion: "0.3.3",
+    lanServerVersion: "0.3.4",
     lanServerUrl: 'https://github.com/EmmyLua/EmmyLua-LanguageServer/releases/download'
 }
