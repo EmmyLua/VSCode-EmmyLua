da30e7968880ff394ecdf3eaf515ebe85e182674
tangzx <love.tangzx@qq.com>
2020-09-05 22:34:52 +0800

    update debugger version

--- build/config.js
+++ build/config.js
@@ -1,5 +1,5 @@
 exports.default = {
-    emmyDebuggerVersion: '1.0.12',
+    emmyDebuggerVersion: '1.0.15',
     emmyDebuggerUrl: 'https://github.com/EmmyLua/EmmyLuaDebugger/releases/download',
     lanServerVersion: "0.3.6",
     lanServerUrl: 'https://github.com/EmmyLua/EmmyLua-LanguageServer/releases/download'
