86aa816593809a406a7c3a5f07d067dfed00a2ab
CppCXY <812125110@qq.com>
2023-07-04 20:18:54 +0800

    0.5.16

--- build/config.js
+++ build/config.js
@@ -1,6 +1,6 @@
 exports.default = {
-    emmyDebuggerVersion: '1.6.0',
+    emmyDebuggerVersion: '1.6.1',
     emmyDebuggerUrl: 'https://github.com/EmmyLua/EmmyLuaDebugger/releases/download',
-    lanServerVersion: "0.5.15",
+    lanServerVersion: "0.5.16",
     lanServerUrl: 'https://github.com/EmmyLua/EmmyLua-LanguageServer/releases/download'
 }
