4f55a8b3a6e99c470388c7f08e22327e61cdc97d
zfl9 <zfl9.com@gmail.com>
2023-08-02 16:03:40 +0800

    Update the prototype of `assert()`, replace `any` with the generic `T`

--- res/std/global.lua
+++ res/std/global.lua
@@ -17,10 +17,11 @@
 --- **false**); otherwise, returns all its arguments. In case of error,
 --- `message` is the error object; when absent, it defaults to "assertion
 --- failed!"
----@overload fun(v:any):any
----@param v any
+---@overload fun(v:T):T
+---@generic T
+---@param v T
 ---@param message string
----@return any
+---@return T
 function assert(v, message) end
 
 ---
