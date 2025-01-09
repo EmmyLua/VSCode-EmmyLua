c5484a55e707049f3196512ed73ff39afb65d8d3
CppCXY <812125110@qq.com>
2025-01-09 19:13:45 +0800

    update

--- build/config.js
+++ build/config.js
@@ -1,6 +1,6 @@
 exports.default = {
     emmyDebuggerVersion: '1.8.2',
     emmyDebuggerUrl: 'https://github.com/EmmyLua/EmmyLuaDebugger/releases/download',
-    newLanguageServerVersion: "0.1.7",
+    newLanguageServerVersion: "0.2.0",
     newLanguageServerUrl: "https://github.com/CppCXY/emmylua-analyzer-rust/releases/download"
 }
