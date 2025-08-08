11d3b0ccb2cfb98f675633eb5c1a3ef659b3d96e
xuhuanzy <501417909@qq.com>
2025-08-08 16:38:44 +0800

    move EmmyrcSchemaContentProvider

--- src/extension.ts
+++ src/extension.ts
@@ -18,6 +18,7 @@ import { get } from './configRenames';
 import * as Annotator from './annotator';
 import { LuaRocksManager } from './luarocks/LuaRocksManager';
 import { LuaRocksTreeProvider, PackageTreeItem } from './luarocks/LuaRocksTreeProvider';
+import { EmmyrcSchemaContentProvider } from './emmyrcSchemaContentProvider';
 interface DebuggerConfig {
     readonly type: string;
     readonly provider: vscode.DebugConfigurationProvider;
@@ -556,59 +557,6 @@ async function checkLuaRocksInstallation(): Promise<void> {
         );
         if (action === 'Install Guide') {
             vscode.env.openExternal(vscode.Uri.parse('https://luarocks.org/#quick-start'));
-        }
-    }
-}
-
-
-
-/**
- * 提供`.emmyrc.json`的 i18n
- */
-class EmmyrcSchemaContentProvider implements vscode.TextDocumentContentProvider {
-    private readonly schemaBaseDir: string;
-
-    constructor(context: vscode.ExtensionContext) {
-        this.schemaBaseDir = path.join(context.extensionPath, 'syntaxes');
-    }
-
-    async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
-        const schemaIdentifier = path.posix.basename(uri.path);
-        const locale = vscode.env.language;
-        let schemaFileName: string;
-
-        if (schemaIdentifier === 'emmyrc') {
-            switch (locale) {
-                case 'zh-cn':
-                case 'zh-CN':
-                case 'zh':
-                    schemaFileName = 'schema.zh-cn.json';
-                    break;
-                case 'en':
-                case 'en-US':
-                case 'en-GB':
-                default:
-                    schemaFileName = 'schema.json';
-                    break;
-            }
-        } else {
-            return '';
-        }
-
-        // 检查schema文件是否存在, 如果不存在则使用默认的
-        let schemaFilePath = path.join(this.schemaBaseDir, schemaFileName);
-        if (!fs.existsSync(schemaFilePath)) {
-            schemaFilePath = path.join(this.schemaBaseDir, 'schema.json');
-        }
-
-        try {
-            return await fs.promises.readFile(schemaFilePath, 'utf8');
-        } catch (error: any) {
-            return JSON.stringify({
-                "$schema": "https://json-schema.org/draft/2020-12/schema#",
-                "title": "Error Loading Schema",
-                "description": `Could not load schema: ${schemaFileName}. Error: ${error.message}.`
-            });
         }
     }
 }
